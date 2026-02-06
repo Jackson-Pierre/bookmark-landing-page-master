310a7bccc75538fbb3372950fc7f32a0fd924b6e
Jackson <jacksonppereira4@gmail.com>
2026-02-06 19:32:54 -0400

    Aba de funcionalidades funcionando

--- script.js
+++ script.js
@@ -4,6 +4,14 @@ const body = document.querySelector('body');
 const btnHamburguer = document.getElementById('btn-hamburguer');
 const nav = document.querySelector('.header-content nav');
 
+const div1 = document.querySelector('.cards-features-one');
+const div2 = document.querySelector('.cards-features-two');
+const div3 = document.querySelector('.cards-features-three');
+
+const btn1 = document.querySelector('.btn-features-one');
+const btn2 = document.querySelector('.btn-features-two');
+const btn3 = document.querySelector('.btn-features-three');
+
 function toggleTheme() {
     const logoTheme = document.querySelector('.logo-theme');
     const logoHeader = document.querySelector('.logo-header');
@@ -21,6 +29,33 @@ function toggleTheme() {
 function openMenu() {
     btnHamburguer.classList.toggle('active');
     nav.classList.toggle('active');
+}
+
+function showFeaturesOne(){
+    btn1.classList.add('active');
+    btn2.classList.remove('active');
+    btn3.classList.remove('active');
+    div1.classList.add('active');
+    div2.classList.remove('active');
+    div3.classList.remove('active');
+}
+
+function showFeaturesTwo(){
+    btn1.classList.remove('active');
+    btn2.classList.add('active');
+    btn3.classList.remove('active');
+    div1.classList.remove('active');
+    div2.classList.add('active');
+    div3.classList.remove('active');
+}
+
+function showFeaturesThree(){
+    btn1.classList.remove('active');
+    btn2.classList.remove('active');
+    btn3.classList.add('active');
+    div1.classList.remove('active');
+    div2.classList.remove('active');
+    div3.classList.add('active');
 }
 
 btnTheme.addEventListener('click', toggleTheme)
